1632559f7776cec9d6c3170ef9193a830a7b2ac0
Paul <p.rossiter83@gmail.noreply.github.com>
2024-02-15 15:25:53 +0000

    [Refactored]links

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Route>
   ),
 );
